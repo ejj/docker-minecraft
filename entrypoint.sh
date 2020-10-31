522ade4ffce6361125115d81376ca056035bee45
Ethan J. Jackson <ethan@kelda.io>
2020-10-30 19:17:17 -0700

    Make the global symlink

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 
 echo Booting World $WORLD
 curl `curl https://www.minecraft.net/en-us/download/server | grep -o https.*server.jar` -o /server.jar
+mkdir -p /mnt/worlds/$WORLD/generated/minecraft/structures
+ln -s /mnt/worlds/global /mnt/worlds/$WORLD/generated/minecraft/structures/ || true
 /usr/bin/java -Xmx6G -jar /server.jar --nogui --universe /mnt/worlds --world $WORLD
